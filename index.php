ffc00bf8ecb44942bb8c664cf2a62af55b8047a5
Johan Castillo <jcjohan2707@gmail.com>
2020-09-08 18:13:37 -0400

    Finish

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+  //Dato a encriptar
+  $password = "123456";
+
+  //Encriptando mediante password_hash()
+  $hash = password_hash($password, PASSWORD_DEFAULT, ['cost' => 10]);
+
+  //Mostrando hash en pantalla
+  echo $hash.'<br>';
+
+  //Validando hash mediante password_verify()
+  if (password_verify($password, $hash)) {
+    echo "El password es correcto";
+  }else{
+    echo "El password es incorrecto";
+  }
+
+?>
